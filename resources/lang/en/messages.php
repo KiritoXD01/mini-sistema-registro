a171e619c3b689f624ba6facd98336570d7bd120
Javier Emmanuel Mercedes <manuelmercedez10@gmail.com>
2020-05-16 02:31:40 -0400

    Added login page

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    'welcome' => 'Welcome',
+    'login' => 'Login',
+    'pleaseEnterEmail' => 'Please Enter Email Address',
+    'pleaseEnterPassword' => 'Please Enter Password',
+    'pleaseWait' => 'Please Wait'
 ];
--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    'welcome' => 'Bienvenid@',
+    'login' => 'Iniciar sesión',
+    'pleaseEnterEmail' => "Por favor, Introduzca su Email",
+    'pleaseEnterPassword' => "Por favor, ingrese su contraseña",
+    'pleaseWait' => 'Por favor espere'
 ];
